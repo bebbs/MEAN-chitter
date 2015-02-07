1a089afe3294388823658076ed76c395ab25ec16
Josh Bebbington <josh@focalwebsites.com>
2015-02-07 14:40:45 +0000

    Node API has Mongo-powered GET and POST endpoints

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Post = require('./models/post')
 
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/posts', function(request,response) {
-  response.json([
-    {
-      username: 'josh',
-      body: 'node rocks!'
-    }
-  ]);
+app.get('/api/posts', function(request,response, next) {
+  Post.find(function(err, posts) {
+    if (err) { return next(err) };
+    response.json(posts);
+  })
 });
 
-app.post('/api/posts', function(request,response) {
-  console.log('Post received:');
-  console.log(request.body.username);
-  console.log(request.body.body);
-  response.send(201);
+app.post('/api/posts', function(request,response, next) {
+  var post = new Post({
+    username: request.body.username,
+    body: request.body.body
+  });
+  post.save(function(err,post) {
+    if (err) { return next(err) };
+    response.json(201, post)
+  });
 })
 
 app.listen(9999, function() {
